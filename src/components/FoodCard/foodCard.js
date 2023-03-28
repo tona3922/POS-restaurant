9f24645e90759ad27262a86dd4d6a975c7297b08
Nice <hoangkimc93@gmail.com>
2023-03-28 18:45:59 +0700

    add menu

---
+++ src/components/FoodCard/foodCard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './foodCard.scss'
+
+const FoodCard = (props) => {
+    console.log('foodCard', props)
+    return (
+        <div className="foodCard">
+            <div className="img-container">
+                <img src={props.src} />
+            </div>
+            <div className="info">
+                <div className="pricetag">
+                    <div className="title">{props.title}</div>
+                    <div className="price">{props.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + "₫"}</div>
+
+                </div>
+                <div className="details">{props.details}</div>
+            </div>
+            <a href="#" className="addtoCart">Thêm</a>
+        </div>
+    )
+}
+
+export default FoodCard
---
+++ src/components/Header/header.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import "./header.scss"
+
+export default function Header() {
+    // Declare a new state variable, which we'll call "count"  const [count, setCount] = useState(0);
+    return (
+        <div className="header">
+            MÓN MỚI
+        </div>
+    );
+}
